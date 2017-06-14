880c7f4732407d51b13a6d872a936dc46c53546a
DESKTOP-F6IJSCT\emens <syauri04@gmail.com>
2017-06-14 23:36:33 +0700

    dfsf

--- application/core/DC_Controller.php
+++ application/core/DC_Controller.php
@@ -143,10 +143,7 @@ class DC_Controller extends CI_Controller {
         $this->email->send();
     }
 
-    function returnJson($msg) {
-        echo json_encode($msg);
-        exit;
-    }
+
 
     function indonesian_date($date)
     {
